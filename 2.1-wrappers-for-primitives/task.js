1fc52a61ae825552cec0773a8611b9f3b7f6f593
roman-krasowski <54819181+roman-krasowski@users.noreply.github.com>
2020-02-18 00:32:34 +0300

    initial commit

--- 2.1-wrappers-for-primitives/task.js
+++ 2.1-wrappers-for-primitives/task.js
@@ -1,9 +1,53 @@
+//Assignment 1
+'use strict'
+function calculateMortgage() {
+    let percent = window.percent.value;
+    let contribution = window.contribution.value;
+    let amount = window.amount.value;
+    let date = window.date.value;
+
+    let result = calculateTotalMortgage(percent, contribution, amount, date);
+    let span = window.mortageResult;
+    span.textContent = result;
+}
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+
+    let dateStart = new Date(),
+        dateEnd = new Date(date);
+    let months = (dateEnd.getFullYear() - dateStart.getFullYear()) * 12;
+    months -= dateStart.getMonth() + 1;
+    months += dateEnd.getMonth();
+
+    let n = months <= 0 ? 0 : months;
+
+    let S = parseInt(amount) - parseInt(contribution),
+        P = parseInt(percent) / 100 / 12;
+
+    let payment = S * (P + P / (((1 + P) ^ n) - 1));
+
+    let totalAmont = payment * n;
+
+    console.log(totalAmont.toFixed(2));
+    return totalAmont.toFixed(2);
+}
+
+
+//Assignment 2
+function sayHello() {
+    let name = window.personName.value;
+    let greeting = getGreeting(name);
+    let span = window.helloResult;
+    span.textContent = greeting;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting;
+
+    if (typeof(name) === 'string' && name !== "undefined" && name !== "null" && name !== "" ){
+        greeting = `Привет, мир! Меня зовут ${name}`;
+    } else {
+        greeting = 'Привет, мир! Меня зовут Аноним';
+    }
+    return greeting;
 }
