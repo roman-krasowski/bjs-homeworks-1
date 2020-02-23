2e8e693bd77763259a74694af100e73d3a737bbc
roman-krasowski <54819181+roman-krasowski@users.noreply.github.com>
2020-02-24 01:18:52 +0300

    assignments 2 and 3 were added

--- 2.2-object-prototype/main.js
+++ 2.2-object-prototype/main.js
@@ -1,27 +1,60 @@
+//Assignment 1
 function initPrintAnimalSound() {
     const animal = {
         sound: 'grrrr',
     };
-
     const result = getAnimalSound(animal);
-
-    document.getElementById('sound').innerHTML = result;   
+    document.getElementById('sound').innerHTML = result;
 }
 
+function getAnimalSound(animal) {
+    if (animal === undefined) {
+        return null;
+    } else {
+        let sound = animal.sound;
+        return sound;
+    }
+}
+
+
+//Assignment 2
 function initCalculateStatement() {
     for (let idx = 0; idx < 3; idx++) {
         const marks = document.getElementById('learner-' + idx).value.split(',');
-
         const average = getAverageMark(marks);
-
         document.getElementById('learner-' + idx + '-average').innerHTML = average;
     }
 }
 
+function getAverageMark(marks) {
+    Array.from(marks);
+    let sum = 0;
+    for (let i = 0; i < marks.length; i++) {
+        marks[i] = parseInt(marks[i]);
+        sum += marks[i];
+    }
+    console.log(marks);
+    let roundedAverage = Math.round(sum / marks.length);
+    return roundedAverage;
+}
+
+
+//Assignment 3
 function initCheckBirthday() {
     const birthday = document.getElementById('birthday').value;
-
     const result = checkBirthday(birthday) ? "Да" : "Нет";
+    document.getElementById('disclaimer').innerHTML = result;
+}
 
-    document.getElementById('disclaimer').innerHTML = result;   
+function checkBirthday(birthday) {
+    let now = Date.now();
+    birthday = Number(new Date(birthday));
+    let diff = now - birthday;
+    let age = diff / 1000 / 60 / 60 / 24 / 365;
+
+    if (age <= 18) {
+        return 'Нет';
+    } else {
+        return 'Да';
+    }
 }
